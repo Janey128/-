312fbbc1f40497c9e50880e8c1d4ea5c192a8b3c
Janey128 <md3n16@soton.ac.uk>
2017-07-31 11:19:36 +0100

    Add files via upload

---
+++ practice1.js
@@ -0,0 +1,24 @@
+/**
+ * Created by dumengzhu on 17/7/22.
+ */
+
+$(document).ready(function(){
+    $("btn1").click(function(){
+        $("d2").css("width","400px");
+    })
+})
+$(document).ready(function(){
+    $("btn2").click(function(){
+        $("d2").css("height","400px");
+    })
+})
+$(document).ready(function(){
+    $("btn3").click(function(){
+        $("d2").css("background-color","red");
+    })
+})
+$(document).ready(function(){
+    $("btn4").click(function(){
+        $("d2").hide();
+    })
+})
